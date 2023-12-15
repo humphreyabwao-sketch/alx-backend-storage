54851a8efce61345935255f13b0d310e1bd6c3b4
humphreyabwao-sketch <humphreyabwao@gmail.com>
2023-12-15 19:45:31 +0300

    initial commit

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,17 @@
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
 BEGIN
-  DECLARE sum_scores DECIMAL(10,2) DEFAULT 0;
-  DECLARE sum_weights INT DEFAULT 0;
-  
-  -- Check if user exists
-  IF NOT EXISTS (SELECT 1 FROM users WHERE id = user_id) THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User does not exist';
-    RETURN;
-  END IF;
-  
-  -- Calculate sum of scores and weights for the user
-  SET sum_scores = 0;
-  SET sum_weights = 0;
-  
-  SELECT SUM(score * weight) AS total_weighted_score, SUM(weight) AS total_weight
-  FROM corrections c
-  INNER JOIN projects p ON c.project_id = p.id
-  WHERE c.user_id = user_id;
-  
-  -- Update user average score if corrections exist
-  IF total_weight > 0 THEN
-    SET sum_scores = total_weighted_score;
-    SET sum_weights = total_weight;
-    
-    UPDATE users SET average_score = sum_scores / sum_weights WHERE id = user_id;
-  END IF;
-END;
+    DECLARE w_avg_score FLOAT;
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight) 
+                        FROM users AS U 
+                        JOIN corrections as C ON U.id=C.user_id 
+                        JOIN projects AS P ON C.project_id=P.id 
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
+END
+$$
+DELIMITER ;
